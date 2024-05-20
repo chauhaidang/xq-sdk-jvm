27b74b8239cec5f93b050e94f20aaa4a7a675a1a
chauhaidang1 <chauhaidang1@gmail.com>
2024-05-20 23:08:00 +0700

    add :common ConfigReader functionality

---
+++ common/src/main/java/com/xq/ConfigReader.java
@@ -0,0 +1,28 @@
+package com.xq;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+    public Properties readConfig(String fileName) {
+        Properties props = new Properties();
+        try (InputStream in = getClass().getClassLoader().getResourceAsStream(fileName)) {
+            props.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException("can't read config", e.getCause());
+        }
+
+        return props;
+    }
+
+    public Properties readConfig() throws IOException {
+        return readConfig(Constant.DEFAULT_CONFIGURATION_FILE);
+    }
+
+    public Config loadConfig(Properties props) {
+        Config conf = new Config();
+        conf.setSdkVersion(props.getProperty("sdk.version"));
+        return conf;
+    }
+}
