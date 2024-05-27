10fb1516d01c3fbfdfa75e72f0185cfb209ad96a
chauhaidang1 <chauhaidang1@gmail.com>
2024-05-27 22:10:50 +0700

    centralize junit & jacoco to xq-base plugin

--- plugins/java-plugins/src/main/kotlin/xq-base.gradle.kts
+++ plugins/java-plugins/src/main/kotlin/xq-base.gradle.kts
@@ -6,3 +6,22 @@ plugins {
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy("jacocoTestReport")
+}
+
+tasks.jacocoTestReport {
+    dependsOn("test")
+}
+
