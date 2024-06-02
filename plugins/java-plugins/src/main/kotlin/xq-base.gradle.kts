feefae470f00274e6c79dd28e700010786861665
chauhaidang1 <chauhaidang1@gmail.com>
2024-06-02 15:51:23 +0700

    add existing tasks and custom tasks

--- plugins/java-plugins/src/main/kotlin/xq-base.gradle.kts
+++ plugins/java-plugins/src/main/kotlin/xq-base.gradle.kts
@@ -1,7 +1,11 @@
+import com.xq.JarCount
+
 plugins {
     id("java")
     id("jacoco")
 }
+
+group = "com.xq"
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
@@ -24,4 +28,33 @@ tasks.test {
 tasks.jacocoTestReport {
     dependsOn("test")
 }
+
+tasks.register<Zip>("xqBundle") {
+    group = "XQ Base Plugins"
+    description = "Bundle jar & files in class path"
+    from(tasks.jar)
+    from(configurations.runtimeClasspath)
+    destinationDirectory.set(layout.buildDirectory.dir("dist"))
+}
+
+tasks.register<JarCount>("xqCountJars") {
+    group = "XQ Base Plugins"
+    description = "Count jars in class path"
+
+    allJars.from(tasks.jar)
+    allJars.from(configurations.runtimeClasspath)
+    countFile.set(layout.buildDirectory.file("gen/count.txt"))
+}
+
+tasks.build {
+    dependsOn(tasks.named("xqBundle"))
+}
+
+tasks.register("xqBuildAll") {
+    group = "XQ Base Plugins"
+    description = "Build all"
+    dependsOn(tasks.named("build"))
+    dependsOn(tasks.named("xqCountJars"))
+}
+
 
