5d01940f5f48f3ed19c54c31c352f1ebe283c466
chauhaidang1 <chauhaidang1@gmail.com>
2024-06-18 22:39:34 +0700

    init service accounts

---
+++ ms-accounts/src/main/java/com/xq/accounts/repository/AccountsRepository.java
@@ -0,0 +1,10 @@
+package com.xq.accounts.repository;
+
+import com.xq.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+
+}
---
+++ ms-accounts/src/main/java/com/xq/accounts/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.xq.accounts.repository;
+
+import com.xq.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
