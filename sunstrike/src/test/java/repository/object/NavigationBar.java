f30e4b546134a3b53251e1296bf707fc55b41835
chauhaidang1 <chauhaidang1@gmail.com>
2025-08-16 22:24:45 +0700

    remove systemtest moduel

--- sunstrike/src/test/java/repository/object/NavigationBar.java
+++
@@ -1,9 +0,0 @@
-package repository.object;
-import org.openqa.selenium.By;
-
-import static com.codeborne.selenide.appium.selector.CombinedBy.android;
-import static com.xq.helper.PlatformLocator.byAndroidResourceId;
-
-public interface NavigationBar {
-    By HOME_NAV = android(byAndroidResourceId("bottom-nav-home"));
-}
